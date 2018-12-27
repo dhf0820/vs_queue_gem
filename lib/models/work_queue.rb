498c3a8afb1390f84a44cc6b595499951f582b7c
Donald French <dhfrench@vertisoft.com>
2018-12-27 12:00:38 -0800

    Added subscribe as method to WorkQueue

--- lib/models/work_queue.rb
+++ lib/models/work_queue.rb
@@ -4,10 +4,14 @@ require 'base64'
 
 #module DemoGem
   class WorkQueue
+    @@connection = nil
 
     def initialize(queue_name)
-      @connection = MqConnection.connection
-      @ch = @connection.create_channel
+      if @@connection.nil?
+        puts "Creating new Rabbit Connection"
+        @@connection = MqConnection.connection
+      end
+      @ch = @@connection.create_channel
       @ch.prefetch(1) # allow for more than one worker
       @queue = @ch.queue(queue_name, :persistent => true, durable: true, 
             auto_delete: false, exclusive: false)
@@ -31,6 +35,21 @@ require 'base64'
                                   :durable => true, :exclusive => false)
     end
     
+    def subscribe(manual: true, block: true)
+      @queue.subscribe(:manual_ack => manual, :block => block) do |delivery_info, properties, body|
+
+        puts "Received from queue: #{body}"
+
+        # if body
+           body = JSON.parse body, symbolize_names: true
+        # end
+        puts "Yielding"
+        yield(body, delivery_info, properties)
+        puts "@@@back from yield\n\n"
+        exit
+      end
+    end 
+
     def queue
       @queue
     end
@@ -60,7 +79,17 @@ require 'base64'
     end
 
     def close_channel
-      @ch.close
+      #@ch.close  # this also closes the connection
+      puts "Clossing Channel"
+      # @@connection.close
+      # @@connection = nil
+    end
+
+    def close
+      #@ch.close
+      puts "Closing Connection"
+      MqConnection.close
+      @@connection = nil
     end
 
     def ack(msg)
--- spec/models/work_queue_spec.rb
+++ spec/models/work_queue_spec.rb
@@ -12,32 +12,38 @@ RSpec.describe WorkQueue do
 
   it 'creates a connection with the server' do
     queue = WorkQueue.new('test_queue')
-    queue.close_channel
+    queue.close
   end
 
   it 'queues a message' do
-    queue = WorkQueue.new('test_queue')
-    data = {}
-    data[:field1] = "Theresa French"
-    val = queue.publish(data)
-    expect(val).to_not be_nil
-    queue.close_channel
-  end
-
-  it "Retrieves a message" do
+    
     queue = WorkQueue.new('test_queue')
     data = {}
     data[:my_love] = "Theresa French"
     val = queue.publish(data)
-    queue.queue.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-      @body = body
+    expect(val).to_not be_nil
+    queue.close
+  end
+
+  it "Retrieves a message" do
+    puts "Retrieves a message from the queue"
+    queue = WorkQueue.new('test_queue')
+    data = {}
+    data[:my_love] = "Theresa French"
+    val = queue.publish(data)
+    puts "calling queue.subscribe"
+    queue.subscribe(manual: true, block: true) do |body, delivery_info, properties|
+      data = body
+      puts " subscribe received: #{data}  :  #{body}"
       queue.ack(delivery_info.delivery_tag)
+      expect(data).to_not be_nil
+      expect(data[:my_love]).to eql "Theresa French"
       exit
     end
     #queue.ack(@delivery_info.delivery_tag)
     sleep 1  
-    expect(@body).to_not be_nil
-    data = JSON.parse(@body)
-    expect(data[:my_love]).to eql "Theresa French"
+    # expect(@body).to_not be_nil
+    # data = JSON.parse(@body)
+    # expect(data[:my_love]).to eql "Theresa French"
   end
 end
