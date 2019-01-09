d0f4f2306c9eb8d802fc636ed89f05e7d3c6c5ea
Donald French <dhfrench@vertisoft.com>
2019-01-09 14:19:21 -0800

    Add ability to add a url to use

--- lib/models/mq_connection.rb
+++ lib/models/mq_connection.rb
@@ -3,17 +3,28 @@
 
 class MqConnection
     @@connection = nil
+    @@url = ENV['VS_AMQP']
 
     def self.connection
       if @@connection.nil?
-        @@connection = Bunny.new(ENV['VS_AMQP'])
+        @@connection = Bunny.new(@@url)
         @@connection.start
       end
       @@connection
     end
   
+    def self.set_url(url)
+      @@url = url
+    end
+
+    def self.current_url
+      @@url
+    end
+    
     def self.close
-      @@connection.close
-      @@connection = nil
+      if @@connection
+        @@connection.close
+        @@connection = nil
+      end
     end
 end
--- spec/lib/models/mq_connection_spec.rb
+++ spec/lib/models/mq_connection_spec.rb
@@ -3,7 +3,7 @@ require './lib/models/mq_connection'
 require 'json'
 
 
-RSpec.describe MqConnection do
+RSpec.describe MqConnection, focus: true do
   before :all do
     @url = "amqp://dhf:Sacj0nhat1@cat.vertisoft.com/ChartArchive"
     ENV['VS_AMQP']=@url
@@ -13,8 +13,13 @@ RSpec.describe MqConnection do
     expect(ENV['VS_AMQP']).to eql @url
   end
 
-  it 'creates a connection with the server' do
+  it "sets the url to custom", focus: true do
+    MqConnection.set_url("testurl")
+    expect(MqConnection.current_url()).to eql "testurl"
+  end
 
+  it 'creates a connection with the server' do
+    MqConnection.set_url("amqp://dhf:Sacj0nhat1@cat.vertisoft.com/ChartArchive")  
     connection = MqConnection.connection
     expect(connection).to_not be_nil
     connection.close
